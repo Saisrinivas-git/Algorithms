40eb49ff430c75689e265721d0d3ea5d44915214
PADALA SAI SRINIVAS <75213522+Saisrinivas-git@users.noreply.github.com>
2022-03-24 18:15:54 +0530

    Update Binary Search Algorithm.py

--- Binary Search Algorithm.py
+++ Binary Search Algorithm.py
@@ -1,11 +1,12 @@
+#Binary Search Algorithm
 arr=[int(i) for i in input().split()]
 # 2 4 5 6 8
 # l   m   h
-target=int(input())#5
+target=int(input()) #5
 l=0
 h=len(arr)-1
-mid=(l+h)>>1
 while(l<=h):
+    mid=(l+h)>>1
     if(arr[mid]<target):
         l=mid+1
     elif(arr[mid]>target):
@@ -15,4 +16,3 @@ while(l<=h):
         break
     else:
         print(False)
-    mid=(l+h)>>1
